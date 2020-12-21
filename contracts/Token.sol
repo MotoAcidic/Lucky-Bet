ff6cacbb9f028da62033fe3895dd0e1cfba70ac5
TFinch <travisfinch01@gmail.com>
2020-12-21 18:59:29 -0500

    stage

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -18,46 +18,54 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/EnumerableSet.sol";
 */
+
 contract Token is ERC20("Lucky Bet", "LBT"), AccessControl {
     using SafeMath for uint256;
 
     bytes32 private constant SETTER_ROLE = keccak256("SETTER_ROLE");
 
-    uint256 public _totalSupply = 50000e18; //50,000
+    uint256 public _totalSupply = 5000000000e18; //5,000,000,000
     
-    address internal _moderator = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-    address internal _owner = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-    
+    address internal _moderator = 0x583031D1113aD414F02576BD6afaBfb302140225;
+    address internal _owner = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
+    address payable internal _contractOwner;
     uint256 internal _teamFund = 0;
+    
+    uint256 moderatorPercent = 100; // 2%
+    uint256 teamPercent = 100; // 1%
+    uint256 ownersPercent = 300; // 3%
+    
+    // 300 point range
+    uint256 smallBetSmallWin = 7000; // 70%
+    uint256 smallBetMediumWin = 9000; // 90%
+    uint256 smallBetBigWin = 14000; // 140%
+        
+    // 200 point range
+    uint256 mediumBetSmallWin = 5000; // 50%
+    uint256 mediumBetMediumWin = 8000; // 80%
+    uint256 mediumBetBigWin = 20000; // 200%
+        
+    // 100 point range
+    uint256 largeBetSmallWin = 3000; // 30%
+    uint256 largeBetMediumWin = 7000; // 70%
+    uint256 largeBetBigWin = 30000; // 300%
+
+    uint256 private _sessionsIds;
+    
     mapping(address => gameData) addressGameHistory;
     mapping(uint256 => gameData) sessionGameHistory;
     
-        uint256 moderatorPercent = 100; // 2%
-        uint256 teamPercent = 100; // 1%
-        uint256 ownersPercent = 300; // 3%
-    
-        // 300 point range
-        uint256 smallBetSmallWin = 70; // .7
-        uint256 smallBetMediumWin = 90; // .9
-        uint256 smallBetBigWin = 140; // 1.4
-        
-        // 200 point range
-        uint256 mediumBetSmallWin = 50; // .5
-        uint256 mediumBetMediumWin = 80; // .8
-        uint256 mediumBetBigWin = 200; // 2
-        
-        // 100 point range
-        uint256 largeBetSmallWin = 30; // .3
-        uint256 largeBetMediumWin = 70; // .7
-        uint256 largeBetBigWin = 300; // 3
-
     struct gameData { 
         address account;
-        uint256 amount; 
-        uint256 number;
-        uint256 loss;
         uint256 session;
+        uint256 amount;
+        uint256 reward;
+        uint256 loss;
+        uint256 teamFee;
+        uint256 luckyNumber;
     }
+    
+
     
     modifier onlySetter() {
         require(hasRole(SETTER_ROLE, _msgSender()), "Caller is not a setter");
@@ -66,30 +74,44 @@ contract Token is ERC20("Lucky Bet", "LBT"), AccessControl {
 
     constructor() public {
         _setupRole(SETTER_ROLE, msg.sender);
-
+        _contractOwner = msg.sender;
         _mint(msg.sender, _totalSupply);
+    }
+    
+    function transferLoss(address account, uint256 amount) internal{
+        transfer(account, amount);
+    }
+    
+    function transferProfit(uint256 amount) public payable{
+        transferFrom(_contractOwner, address(this), amount);
+        
     }
 
     function getSetterRole() external pure returns (bytes32) {
         return SETTER_ROLE;
     }
 
-    function getBalance(address account) public view {
+    function getBalance(address account) public view returns (uint256){
         balanceOf(account);
     }
 
-    function luckyBet(uint256 amount) public {
-        require(amount >= 2, "Cannot stake less than 5 LBT");
+    function luckyBet(uint256 amount) public payable{
+        require(amount >= 2, "Cannot stake less than 2 LBT");
         require(amount <= 100, "Cannot stake more than 100 LBT");
         
+        _sessionsIds = _sessionsIds.add(1);
         
+        uint256 sessionId = _sessionsIds;
         uint256 luckyNumber;
         luckyNumber = rand();
+        uint256 totalFees;
         uint256 reward;
         uint256 loss;
+        uint256 profit;
         uint256 moderatorCut;
         uint256 ownerCut;
-        uint256 teamCut;
+        //uint256 teamCut;
+        uint256 feeAfterCuts;
 
         
         // ------------------------------------------------------------------------
@@ -97,15 +119,18 @@ contract Token is ERC20("Lucky Bet", "LBT"), AccessControl {
         // ------------------------------------------------------------------------
         if(amount >= 2 && amount <= 10){
             // Small win
-            if(luckyNumber <= 1000 && luckyNumber >= 900 || luckyNumber <= 100 && luckyNumber > 0){
-                reward = percentageOf(amount, smallBetBigWin);
+            if(luckyNumber >= 900 || luckyNumber <= 100 ){
+                reward = amount.mul(smallBetBigWin).div(10000);
                 loss = 0;
+                profit = reward.sub(amount);
             }else if (luckyNumber >= 800 || luckyNumber <= 200){
-                reward = percentageOf(amount, smallBetMediumWin);
-                loss = amount.sub(smallBetMediumWin);
-            }else if(luckyNumber >= 700 || luckyNumber <= 300){
-                reward = percentageOf(amount, smallBetSmallWin);
-                loss = amount.sub(smallBetSmallWin);
+                reward = amount.mul(smallBetMediumWin).div(10000);
+                loss = amount.sub(reward);
+                profit = 0;
+            }else if(luckyNumber >= 700 || luckyNumber <= 600){
+                reward = amount.mul(smallBetSmallWin).div(10000);
+                loss = amount.sub(reward);
+                profit = 0;
             }
         }
         
@@ -115,14 +140,14 @@ contract Token is ERC20("Lucky Bet", "LBT"), AccessControl {
         if(amount >= 11 && amount <= 50){
             // Small win
             if(luckyNumber >= 900 || luckyNumber <= 100 ){
-                reward = percentageOf(amount, mediumBetBigWin);
+                reward = amount.mul(mediumBetBigWin).div(10000);
                 loss = 0;
             }else if (luckyNumber >= 800 || luckyNumber <= 200){
-                reward = percentageOf(amount, mediumBetMediumWin);
-                loss = amount.sub(mediumBetMediumWin);
-            }else if(luckyNumber >= 700 || luckyNumber <= 300){
-                reward = percentageOf(amount, mediumBetSmallWin);
-                loss = amount.sub(mediumBetSmallWin);
+                reward = amount.mul(mediumBetMediumWin).div(10000);
+                loss = amount.sub(reward);
+            }else if(luckyNumber >= 700 || luckyNumber <= 600){
+                reward = amount.mul(mediumBetSmallWin).div(10000);
+                loss = amount.sub(reward);
             }
         }
         
@@ -132,35 +157,68 @@ contract Token is ERC20("Lucky Bet", "LBT"), AccessControl {
         if(amount >= 51 && amount <= 100){
             // Small win
             if(luckyNumber >= 900 || luckyNumber <= 100 ){
-                reward = percentageOf(amount, largeBetBigWin);
+                reward = amount.mul(mediumBetBigWin).div(10000);
                 loss = 0;
             }else if (luckyNumber >= 800 || luckyNumber <= 200){
-                reward = percentageOf(amount, largeBetMediumWin);
-                loss = amount.sub(largeBetMediumWin);
-            }else if(luckyNumber >= 700 || luckyNumber <= 300){
-                reward = percentageOf(amount, largeBetSmallWin);
-                loss = amount.sub(largeBetSmallWin);
+                reward = amount.mul(mediumBetBigWin).div(10000);
+                loss = amount.sub(reward);
+            }else if(luckyNumber >= 700 || luckyNumber <= 600){
+                reward = amount.mul(mediumBetBigWin).div(10000);
+                loss = amount.sub(reward);
             }
         }
         
-            loss = amount.sub(reward);
-            moderatorCut = percentageOf(loss, moderatorPercent);
-            teamCut = percentageOf(loss, teamPercent);
-            ownerCut = percentageOf(loss, ownersPercent);
+        
+        if (profit > 0){
+            transferFrom(_contractOwner, msg.sender, profit);
+        }else if(profit < 1 && moderatorCut < 1 && ownerCut < 1){
+            transfer(_contractOwner, loss);
+        }else if(profit < 1 && moderatorCut >= 1 && ownerCut >= 1){
             
-            // Transfer reward
-            Transfer(address(0), msg.sender, reward);
+            moderatorCut = loss.mul(moderatorPercent).div(10000);
             
-            transfer(_moderator, moderatorCut);
+            ownerCut = loss.mul(ownersPercent).div(10000);
+        
+            totalFees = moderatorCut.add(ownerCut);
+            feeAfterCuts = loss.sub(totalFees);
+        
+            transferLoss(_moderator, moderatorCut);
             transfer(_owner, ownerCut);
+            transfer(_contractOwner, feeAfterCuts);
+        }
+
+        
+        gameData memory gameData_ = gameData({
+            account: msg.sender,
+            session: sessionId,
+            amount: amount,
+            reward: profit,
+            loss: loss,
+            teamFee: totalFees,
+            luckyNumber: luckyNumber
+        });  
+        
+        addressGameHistory[msg.sender] = gameData_;
+        sessionGameHistory[sessionId] = gameData_;
 
     }
     
-    function returnSessionInfo(uint256 sessionID) public view returns (address account, uint256 amount, uint256 loss, uint256 session){
+    function returnSessionInfo(uint256 sessionID) public view returns (
+        address account, 
+        uint256 session, 
+        uint256 amount, 
+        uint256 reward,
+        uint256 loss,
+        uint256 teamFee,
+        uint256 luckyNumber){
         return (sessionGameHistory[sessionID].account,
+                sessionGameHistory[sessionID].session,
                 sessionGameHistory[sessionID].amount,
+                sessionGameHistory[sessionID].reward,
                 sessionGameHistory[sessionID].loss,
-                sessionGameHistory[sessionID].session
+                sessionGameHistory[sessionID].teamFee,
+                sessionGameHistory[sessionID].luckyNumber
+                
         );
     }
 
@@ -177,5 +235,61 @@ contract Token is ERC20("Lucky Bet", "LBT"), AccessControl {
     
     function percentageOf(uint amount, uint basisPoints) internal pure returns (uint) {
         return amount.mul(basisPoints).div(10000);
+    }
+        /**
+     * @dev bankersRoundedDiv method that is used to divide and round the result 
+     * (AKA round-half-to-even)
+     *
+     * Bankers Rounding is an algorithm for rounding quantities to integers, 
+     * in which numbers which are equidistant from 
+     * the two nearest integers are rounded to the nearest even integer. 
+     *
+     * Thus, 0.5 rounds down to 0; 1.5 rounds up to 2. 
+     * Other decimal fractions round as you would expect--0.4 to 0, 0.6 to 1, 1.4 to 1, 1.6 to 2, etc. 
+     * Only x.5 numbers get the "special" treatment.
+     * @param a What to divide
+     * @param b Divide by this number
+     */
+    function bankersRoundedDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "div by 0"); 
+
+        uint256 halfB = 0;
+        if ((b % 2) == 1) {
+            halfB = (b / 2) + 1;
+        } else {
+            halfB = b / 2;
+        }
+        bool roundUp = ((a % b) >= halfB);
+
+        // now check if we are in the center!
+        bool isCenter = ((a % b) == (b / 2));
+        bool isDownEven = (((a / b) % 2) == 0);
+
+        // select the rounding type
+        if (isCenter) {
+            // only in this case we rounding either DOWN or UP 
+            // depending on what number is even 
+            roundUp = !isDownEven;
+        }
+
+        // round
+        if (roundUp) {
+            return ((a / b) + 1);
+        }else{
+            return (a / b);
+        }
+    }
+    
+     /**
+     * @dev Division, round to nearest integer (AKA round-half-up)
+     * @param a What to divide
+     * @param b Divide by this number
+     */
+    function roundedDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity automatically throws, but please emit reason
+        require(b > 0, "div by 0"); 
+
+        uint256 halfB = (b % 2 == 0) ? (b / 2) : (b / 2 + 1);
+        return (a % b >= halfB) ? (a / b + 1) : (a / b);
     }
 }
